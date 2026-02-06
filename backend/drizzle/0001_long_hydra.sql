cc2950c7665542f877ce834bee1e3a5c225eeae5
Shenley <582783985@qq.com>
2026-02-06 13:12:16 +0800

    refactor: split memos from posts and update database schema

--- backend/drizzle/0001_long_hydra.sql
+++ backend/drizzle/0001_long_hydra.sql
@@ -1,14 +1,21 @@
 CREATE TABLE `memos` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`content` text NOT NULL,
-	`isPublished` integer DEFAULT false NOT NULL,
-	`authorId` integer,
-	`deletedAt` integer,
-	`createdAt` integer DEFAULT (unixepoch()) NOT NULL,
-	`updatedAt` integer DEFAULT (unixepoch()) NOT NULL,
-	FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE set null
+    `id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `content` text NOT NULL,
+    `isPublished` integer DEFAULT false NOT NULL,
+    `authorId` integer,
+    `deletedAt` integer,
+    `createdAt` integer DEFAULT (unixepoch()) NOT NULL,
+    `updatedAt` integer DEFAULT (unixepoch()) NOT NULL,
+    FOREIGN KEY (`authorId`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE set null
 );
+
+INSERT INTO `memos` (`content`, `isPublished`, `authorId`, `createdAt`, `updatedAt`)
+SELECT `content`, `isPublished`, `authorId`, `createdAt`, `updatedAt`
+FROM `posts`
+WHERE `type` = 'memo';
+
 --> statement-breakpoint
-CREATE INDEX `memosCreatedAtIdx` ON `memos` (`createdAt`);--> statement-breakpoint
-CREATE INDEX `memosIsPublishedIdx` ON `memos` (`isPublished`);--> statement-breakpoint
+
+DELETE FROM `posts` WHERE `type` = 'memo';
+
 ALTER TABLE `posts` DROP COLUMN `type`;
